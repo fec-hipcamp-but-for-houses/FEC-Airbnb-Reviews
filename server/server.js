d26d4a5933e38106630357c83a71f127757c0be7
Zachary <zachary.15pierce@gmail.com>
2019-05-30 16:26:09 -0700

    Dockerfile added

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const app = require('./index.js');
 
 const PORT = 3004;
-
+console.log('WE MADE IT TO SERVER FROM SPIN UP!!!')
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
